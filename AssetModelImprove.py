f9273a3b597791a38180d5fe622f75a3bf49a2d9
1084848158@qq.com <52781018zou>
2018-11-07 16:58:08 +0800

    优化模型研究接口

--- AssetModelImprove.py
+++ AssetModelImprove.py
@@ -11,47 +11,41 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
+from datetime import datetime
 
 class AssetModelImprove:
     def __init__(self):
         self.PrintInfoDemo = PrintInfo()
-        pass
 
-    def Research_MaxDown(self):
-        modelResult = {}
-        fundPortfolioDemo = fundPortfolio()
-        for rate in np.linspace(start=0,stop=0.5,num=5):
-            modelResult['rate=' + str(rate)] = fundPortfolioDemo.setMain(method='target_maxdown', rate=rate)
-
-    def Research_TargetRisk(self):
+    def ResearchModel(self,calcNum=50,methond='target_risk'):
         modelResult = {}
         fundPortfolioDemo = fundPortfolio()
         timeBack = 0
-        for rate in np.linspace(start=0, stop=0.8, num=50):
-            timeBack +=1
-            self.PrintInfoDemo.PrintLog('回测第%s次'%str(timeBack))
-            modelResult['rate=' + str(rate)] = fundPortfolioDemo.setMain(method='target_risk', rate=rate)
-        pickleFile = open('modelResult0_0.5z.pkl','wb')
-        pickle.dump(modelResult,pickleFile)
+        for rate in np.linspace(start=0, stop=1, num=calcNum):
+            timeBack += 1
+            self.PrintInfoDemo.PrintLog('回测第%s次' % str(timeBack))
+            modelResult['rate=' + str(rate)] = fundPortfolioDemo.setMain(method=methond, rate=rate)
+        pickleFile = open(methond+'modelResult.pkl', 'wb')
+        pickle.dump(modelResult, pickleFile)
         pickleFile.close()
         return modelResult
 
-    def calcResearch(self):
+
+    def calcResearch(self,method):
         try:
-            fileResult = open('modelResult0_0.5z.pkl','rb')
+            fileResult = open(method+'modelResult.pkl','rb')
             modelResult = pickle.load(fileResult)
         except:
-            modelResult = self.Research_TargetRisk()
+            modelResult = self.ResearchModel(method=method)
 
         dflist = []
         dfReturnAndRisk = {}
         for keyRate in modelResult:
-            tempSe = modelResult[keyRate]['pofolioAndBench']['IndexPortfolio']
+            tempSe = modelResult[keyRate]['pofolioAndBench'][u'投资组合']
             tempSe.name = keyRate
             dflist.append(tempSe)
-
-            dfReturnAndRisk[keyRate] = modelResult[keyRate]['riskReturndf']['IndexPortfolio'][u'近一年'].to_dict()
-        dflist.append(modelResult[keyRate]['pofolioAndBench']['000300.SH'])
+            dfReturnAndRisk[keyRate] = modelResult[keyRate]['riskReturndf'][u'投资组合'][u'成立以来'].to_dict()
+        dflist.append(modelResult[keyRate]['pofolioAndBench'][u'60.0%沪深300+40.0%中债国债总财富指数'])
 
         sortReturn = sorted(dfReturnAndRisk.items(),key=lambda item:item[0])
         rateList = []
@@ -70,14 +64,26 @@ class AssetModelImprove:
         df = pd.DataFrame([annualReturn,annualStd,maxDown,sharpRate,calmaRate],columns=rateList,
                           index=['annualReturn','annualStd','maxDown','sharpRate','calmaRate']).T
 
-        df['annualStd'].plot()
-        plt.show()
+
+        fig = plt.figure(figsize=(16,9))
+        ax1 = fig.add_subplot(111)
+        df['sharpRate'].plot(ax=ax1,style='r+')
+
 
         portFolioDf = pd.concat(dflist, axis=1)
-        (1+portFolioDf.ix[:,[2,12,22,32,42,-1]]).cumprod().plot()
+        dateList = [datetime.strptime(dateStr,'%Y-%m-%d') for dateStr in portFolioDf.index]
+        portFolioDf = pd.DataFrame(portFolioDf.values,index=dateList,columns=portFolioDf.columns)
+
+        fig = plt.figure(figsize=(16, 9))
+        ax2 = fig.add_subplot(111)
+        (1+portFolioDf.ix[:,[2,12,22,32,42,-1]]).cumprod().plot(ax=ax2)
+        ax2.set_title(u'不同rate走势对比图')
         plt.show()
 
 
 if __name__=='__main__':
+    import time
+    aa = time.time()
     AssetModelImproveDemo = AssetModelImprove()
-    AssetModelImproveDemo.Research_TargetRisk()
+    AssetModelImproveDemo.calcResearch(method='risk_parity')
+    print(u'总耗时：',time.time()-aa)
